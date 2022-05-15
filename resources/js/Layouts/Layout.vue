844bba329c2de00fe4d355017f64bbaba0ba696d
Ruslan_17 <rus.sayko17@gmail.com>
2022-05-15 14:00:17 +0300

    flash message z-index

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mx-auto min-h-full flex flex-col">
-<!--        <FlashMessage position="right top" time="3000"/>-->
+        <FlashMessage position="right top" time="10000"/>
         <Header />
 
         <main class="shrink grow basis-auto">
@@ -14,6 +14,9 @@
 <style>
     html, #app {
         height: 100%;
+    }
+    ._vue-flash-msg-body {
+        z-index: 9999;
     }
 </style>
 
